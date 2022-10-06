8b0f13bda524a92e11e5cd07856d88e45c5c06db
sanpuyuepo <sanpuyuepo_scu@163.com>
2022-10-06 17:30:22 +0800

    test

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -2,7 +2,7 @@
   <div class="home-page">
     <div class="banner">
       <div class="container">
-        <h1 class="logo-font">conduit miura</h1>
+        <h1 class="logo-font">miura-conduit</h1>
         <p>A place to share your knowledge.</p>
       </div>
     </div>
