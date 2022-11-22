0e262d7ccbdd9c9eb5ed7e37b8946c6e842a8080
sanpuyuepo <sanpuyuepo_scu@163.com>
2022-11-22 15:35:48 +0800

    change baseURL

--- plugins/request.js
+++ plugins/request.js
@@ -2,12 +2,12 @@
  * 基于 axios 二次封装请求模块
  */
 
-import axios from 'axios'
+import axios from "axios";
 
 export const request = axios.create({
-  // baseURL: 'https://conduit.productionready.io/api'
-  baseURL: 'http://realworld.api.fed.lagounews.com/api'
-})
+  baseURL: "https://conduit.productionready.io/api",
+  // baseURL: 'http://realworld.api.fed.lagounews.com/api'
+});
 
 // 插件导出函数必须作为 default 成员
 export default ({ store }) => {
@@ -16,15 +16,15 @@ export default ({ store }) => {
   request.interceptors.request.use(
     function (config) {
       // Do something before request is sent
-      const { user } = store.state
+      const { user } = store.state;
       if (user && user.token) {
-        config.headers.Authorization = `Token ${user.token}`
+        config.headers.Authorization = `Token ${user.token}`;
       }
-      return config
+      return config;
     },
     function (error) {
       // Do something with request error
-      return Promise.reject(error)
+      return Promise.reject(error);
     }
-  )
-}
+  );
+};
